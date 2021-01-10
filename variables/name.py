c7042bb08bd6ff873812c7daeb7bbb901f56dadf
IIHARUKAII <sornsiri.kan@ku.th>
2021-01-10 11:08:45 +0700

    add new 3 files in variables folder

---
+++ variables/name.py
@@ -0,0 +1,15 @@
+#สามารถประกาศได้
+myvar = "sorn"
+my_var = "sorn"
+my_var_ "sorn"
+myVar = "sorn"
+MYVAR = "sorn"
+myvar2 = "sorn"
+
+#ไม่สามารถประกาศได้
+2myvar = "sorn"
+my-var = "sorn"
+my var = "sorn"
+
+
+
