2682a31084fe2e5dcaca671dc6a3736d0376b9e1
IIHARUKAII <sornsiri.kan@ku.th>
2021-01-10 10:15:18 +0700

    add new folder and create 3 files in this folder

---
+++ variables/type.py
@@ -0,0 +1,13 @@
+name = "Sornsiri kanoungkid"
+age = 26
+weight = 49.8
+
+# boolean = can, has, is + verb
+can_walk = True
+can_run = True
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
