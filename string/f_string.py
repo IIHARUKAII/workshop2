a1b0ef3e6fb159ebe79b2c0c269ae5134463afbb
IIHARUKAII <sornsiri.kan@ku.th>
2021-01-16 11:14:09 +0700

    add 6 file

---
+++ string/f_string.py
@@ -0,0 +1,6 @@
+name = "sorn"
+age = 26
+
+result = f" My name is {name}, and I am {age}"
+
+print("result", result)  # Output: My name is sorn and I am 26
---
+++ string/format.py
@@ -0,0 +1,4 @@
+age = 26
+txt = "My name is sorn, and i am {}"
+result = txt.format(age)
+print("result", result)  # Output: My name is sorn and I am 26
