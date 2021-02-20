e2ae1cbfc7d167b53cdf93b23428a1c83ed050d8
IIHARUKAII <sornsiri.kan@ku.th>
2021-02-20 09:31:45 +0700

    fix for loop exercise

--- for_loop/for_loop_exercise.py
+++ for_loop/for_loop_exercise.py
@@ -1,10 +1,8 @@
 start = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-end = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-index = 2
+end = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 for first in start:
-    print("\t", index, "\n")
+    print("\t", start, "\n")
     for last in end:
         print(first, "*", last, "=", first * last)
     print("----------------")
-    index += 1
